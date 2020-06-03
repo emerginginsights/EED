18b480f1ed8ab0c99fbe81a70e05ecbd223ddbe3
Serj Klimenko <klimenko.serj@gmail.com>
2020-06-03 13:20:43 +0300

    migrate frontend to  311957cb2ef5b6e6fd1104f9cd494e8ca272be2b EED-FrontEnd (remotes/origin/HEAD)

--- frontend/js/main.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-let menuBlock = document.querySelector('.menu__block');
-let burger = document.querySelector('.burger');
-
-burger.addEventListener('mouseover', () => {
-	menuBlock.classList.toggle('menu__block__active');
-})
-
--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import os
 from eed import create_app
 
 frontend_folder_path = os.path.join(
-    os.path.dirname(os.path.realpath(__file__)), 'frontend')
+    os.path.dirname(os.path.realpath(__file__)), 'EED-FrontEnd')
 
 app = create_app(static_folder=frontend_folder_path, static_url_path='')
 
